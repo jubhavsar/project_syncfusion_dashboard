0c6cf4d69dc2c62bc6fe0d755df8bda65665ae80
jubhavsar <juhi_bhavsar@yahoo.com>
2022-07-25 10:19:08 -0400

    add first toolkit using syncfusion and setting icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
-import { TooltipComponents } from '@syncfusion/ej2-react-popups';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css'
 
 const app = () => {
   return (
-    <h1 className='text-3xl underline'>App</h1>
-  )
+    <div>
+      <BrowserRouter>
+        <div className="relative flex dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zindex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                style={{ background: 'blue', borderRadius: "50%" }}
+                className="p-3 text-3xl text-white hover:drop-shadow-xl hover:bg-light-gray">
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default app
