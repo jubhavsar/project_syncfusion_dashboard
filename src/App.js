5b435cf7988ba8881ccfeca1d9f6227602e2e207
jubhavsar <juhi_bhavsar@yahoo.com>
2022-07-26 11:13:10 -0400

    declare routes for charts

--- src/App.js
+++ src/App.js
@@ -56,7 +56,15 @@ const app = () => {
                 <Route path="/calendar" element="Calendar" />
                 <Route path="/color-picker" element="ColorPicker" />
 
-                
+                {/* charts  */}
+                <Route path="/line" element="Line" />
+                <Route path="/area" element="Area" />
+                <Route path="/bar" element="Bar" />
+                <Route path="/pie" element="Pie" />
+                <Route path="/financial" element="Financial" />
+                <Route path="/color-mapping" element="ColorMapping" />
+                <Route path="/pyramid" element="Pyramid" />
+                <Route path="/stacked" element="Stacked" />
 
                
             </Routes>
